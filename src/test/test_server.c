ca98f92793f02714d23c9411bed96561e288f87f
dafeng996 <1236192@qq.com>
2023-05-20 17:41:31 +0800

    :fire:

--- src/test/test_server.c
+++ src/test/test_server.c
@@ -9,18 +9,24 @@
 
 #pragma comment(linker,"/subsystem:windows")
 
+static HBITMAP hbitmap;
+static int cx_client, cy_client, cx_source, cy_source;
+BITMAP bitmap;
+
+void OnPaint(HWND hwnd);
+
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wparam, LPARAM lparam);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     SetConsoleOutputCP(CP_UTF8);
-    printf("卧槽");
-    const wchar_t class_name[] = L"my window";
+    const wchar_t class_name[] = L"Window";
 
     // 配置窗口类
     WNDCLASS wc = { 0 };
     wc.lpfnWndProc = WindowProc;
     wc.hInstance = hInstance;
     wc.lpszClassName = (LPCWSTR) class_name;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
 
     // 注册窗口类
     RegisterClass(&wc);
@@ -29,7 +35,7 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     HWND hwnd = CreateWindowEx(
             0,
             class_name,
-            (LPCWSTR) L"卧槽",
+            (LPCWSTR) L"测试",
             WS_OVERLAPPEDWINDOW,
             CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
             NULL,
@@ -42,8 +48,9 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         return 0;
     }
 
-    // 显示窗口
+    // 显示&更新窗口
     ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
 
     // 消息循环
     MSG msg = { 0 };
@@ -57,16 +64,45 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wparam, LPARAM lparam) {
     switch (uMsg) {
+        case WM_CREATE:
+            hbitmap = (HBITMAP) LoadImage(
+                    NULL,
+                    L"C:\\Users\\cheng\\workspaces\\tomato-desktop-windows\\cmake-build-debug-visual-studio\\capture.bmp",
+                    IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+            GetObject(hbitmap, sizeof(BITMAP), &bitmap);
+            cx_source = bitmap.bmWidth;
+            cy_source = bitmap.bmHeight;
+            return 0;
         case WM_DESTROY:
             PostQuitMessage(0);
             return 0;
         case WM_PAINT: {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hwnd, &ps);
-            FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW + 1));
-            EndPaint(hwnd, &ps);
+            OnPaint(hwnd);
             return 0;
         }
+        case WM_SIZE:
+            cx_client = LOWORD(lparam);
+            cy_client = HIWORD(lparam);
+        default:
+            return DefWindowProc(hwnd, uMsg, wparam, lparam);
     }
-    return DefWindowProc(hwnd, uMsg, wparam, lparam);
+}
+
+void OnPaint(HWND hwnd) {
+    int x, y;
+    PAINTSTRUCT ps;
+
+    HDC hdc = BeginPaint(hwnd, &ps);
+    HDC mem_dc = CreateCompatibleDC(hdc);
+    SelectObject(mem_dc, hbitmap);
+
+    for (y = 0; y < cy_client; y += cy_source) {
+        for (x = 0; x < cx_client; x += cx_source) {
+            BitBlt(hdc, x, y, cx_source, cy_source, mem_dc, 0, 0, SRCCOPY);
+        }
+    }
+
+    DeleteDC(mem_dc);
+    DeleteDC(hdc);
+    EndPaint(hwnd, &ps);
 }
